4c5ac73282533d059f09d0af22e45a10cb26919a
jjosaez@gmail.com <jjosaez@gmail.com>
2021-07-11 11:17:25 +0200

    corrigiendo campos de modelo de usuario, quitando campos no necesarios

--- app_pedidos_django/applications/usuarios/managers.py
+++ app_pedidos_django/applications/usuarios/managers.py
@@ -4,20 +4,22 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager, models.Manager):
     
-    def _create_user(self, email, password, is_staff, is_superuser,is_active, **extra_fields):
+    def _create_user(self,username, email, password, is_staff, is_superuser, is_active, **extra_fields):
         user = self.model(
+            username = username,
             email=email,
             is_staff=is_staff,
             is_superuser = is_superuser,
             is_active = is_active,
             **extra_fields
         )
+        # Se encripta la contraseña
         user.set_password(password)
         user.save(using=self.db)
         return user
 
-    def create_user (self, email, password=None, **extra_fields):
-        return self._create_user(email, password, False,False, True, **extra_fields)
+    def create_user (self,username, email, password=None, **extra_fields):
+        return self._create_user(username, email, password, False,False, True, **extra_fields)
 
-    def create_superuser(self, email, password = None, **extrafields):
-        return self._create_user(email, password,True, True, **extrafields)
+    def create_superuser(self, username, email, password = None, **extrafields):
+        return self._create_user(username, email, password,True, True, True, **extrafields)
