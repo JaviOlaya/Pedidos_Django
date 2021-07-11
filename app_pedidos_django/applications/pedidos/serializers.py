cfd62a34f1b46603052573e8ccb214526170f1d3
jjosaez@gmail.com <jjosaez@gmail.com>
2021-07-11 09:54:04 +0200

    creando serializers y managers de pedidos 2

--- app_pedidos_django/applications/pedidos/serializers.py
+++ app_pedidos_django/applications/pedidos/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Order
+from .models import Order, OrderDetail
 
 class OrderSerializer(serializers.ModelSerializer):
 
@@ -9,6 +9,7 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields =(
+            'id',
             'date_order',
             'count' ,
             'amount' ,
@@ -16,7 +17,18 @@ class OrderSerializer(serializers.ModelSerializer):
             'close',
             'anulate', 
             'user ',
+            'products',
             )
     
     def get_products(self, obj):
-        query = OrderDetail.objects.products_by_order()
+        query = OrderDetail.objects.products_by_order(obj.id)
+        orders_produced = OrderDetailSerializer(query, many=True)
+        return orders_produced
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderDetail
+        fields = (
+           '__all__'
+        )
